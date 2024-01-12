abe9a2f709e03c6486b032b3c59f666947c40f94
Nathan Hayfield <nhayfield@yahoo.com>
2024-01-12 17:52:19 +0100

    more changes

--- lib/cjs/types/utils.d.ts
+++ lib/cjs/types/utils.d.ts
@@ -2,7 +2,7 @@ import * as jose from 'jose';
 export declare const getClientJwt: () => Promise<string>;
 export declare const parseJWT: (token: string) => jose.JWTPayload;
 export declare const getJWKsData: (baseUrl: string) => Promise<jose.JSONWebKeySet>;
-export declare const verifyPomeriumJWT: (jwt: string, authenticateBaseUrl: string, issuer: string, audience: string | string[]) => Promise<jose.JWTVerifyResult & jose.ResolvedKey<jose.KeyLike>>;
+export declare const verifyPomeriumJWT: (jwt: string, authenticateBaseUrl: string, issuer: string, audience: string | string[]) => Promise<jose.JWTVerifyResult<jose.JWTPayload> & jose.ResolvedKey<jose.KeyLike>>;
 export declare const withHttps: (url: string) => string;
 export declare const signOut: (redirectUrl?: string) => void;
 //# sourceMappingURL=utils.d.ts.map
--- lib/cjs/types/verifier.d.ts
+++ lib/cjs/types/verifier.d.ts
@@ -12,8 +12,8 @@ export declare class PomeriumVerifier {
     jwtData: jose.JWTPayload;
     verifiedJwtData: jose.JWTPayload;
     constructor({ issuer, audience, expirationBuffer }: verifierConfig);
-    verifyBrowserUser(): Promise<jose.JWTVerifyResult & jose.ResolvedKey<jose.KeyLike>>;
-    verifyJwt(jwt: string): Promise<jose.JWTVerifyResult & jose.ResolvedKey<jose.KeyLike>>;
+    verifyBrowserUser(): Promise<jose.JWTVerifyResult<jose.JWTPayload> & jose.ResolvedKey<jose.KeyLike>>;
+    verifyJwt(jwt: string): Promise<jose.JWTVerifyResult<jose.JWTPayload> & jose.ResolvedKey<jose.KeyLike>>;
     tofu(): void;
     audToArray(aud: string | string[]): string[];
     isLoggedIn(): boolean;
