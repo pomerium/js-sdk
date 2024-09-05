d398d74d8faba92e61543edab8deac7e12b4eca9
Kenneth Jenkins <51246568+kenjenkins@users.noreply.github.com>
2024-09-05 10:57:43 -0700

    revert example update
    This won't actually build until we publish a new release and update the
    js-sdk dependency.

--- examples/react-app/src/App.js
+++ examples/react-app/src/App.js
@@ -1,19 +1,23 @@
 import { useEffect, useState } from 'react';
-import { getBrowserUser, signOut } from '@pomerium/js-sdk';
+import { PomeriumVerifier, signOut } from '@pomerium/js-sdk';
 
 function App() {
 
-  const [userInfo, setUserInfo] = useState('');
+  const [jwt, setJwt ] = useState('');
 
   useEffect(() => {
-    getBrowserUser()
-      .then(u => setUserInfo(u))
+    const jwtVerifier = new PomeriumVerifier({
+      audience: 'react.localhost.pomerium.io',
+      expirationBuffer: 1000
+    });
+    jwtVerifier.verifyBrowserUser()
+      .then(r => setJwt(r))
       .catch(e => console.log(e));
   }, [])
 
   return (
     <div style={{margin: '20px'}}>
-      <pre>{JSON.stringify(userInfo, null, 2)}</pre>
+      <pre>{JSON.stringify(jwt, null, 2)}</pre>
       <div style={{marginTop: '20px'}}>
         <button onClick={() => signOut('https://www.pomerium.io')} type="button">Sign Out Test</button>
       </div>
